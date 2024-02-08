2996680356fd957f3be21f67584ac55562bd5525
ALBERT IMASUEN <studentxapp@gmail.com>
2024-02-08 15:21:41 -0500

    Creating SignUp Page Api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
+
+export const Signup = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if(!username || !email || !password || username === '' || email === '' || password === ''){
+        return res.status(400).json({ message: "All Fields are Required"})
+    }
+    const hashPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({
+        username,
+        email,
+        password: hashPassword,
+    });
+
+    await newUser.save();
+    res.json({ message: 'Signup successful'})
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ then(() => {
     console.log(err);
 });
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on part 3000');
@@ -24,3 +26,4 @@ app.listen(3000, () => {
 //Creating Routes
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
